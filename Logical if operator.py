4e18aa1dc39a5bb88c5492947906ffec32b0c8ca
Artem Stoliar <99268175+Tmarbt@users.noreply.github.com>
2022-12-06 18:32:29 +0100

    Create Logical if operator.py

---
+++ Logical if operator.py
@@ -0,0 +1,13 @@
+#tasks from:https://younglinux.info/python/if
+# #1st task
+get_data = input("Please, write something below:\n")
+if not get_data == "":
+    print("Ok")
+#2nd taask
+user_data = int(input("Please, write number here: "))
+if user_data >= 0:
+    print("1")
+else:
+    print("-1")
+
+
