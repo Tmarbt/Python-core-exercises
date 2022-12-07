d5cfdf13675a352275a9d664d3e2ed2f6ff6f121
Artem Stoliar <99268175+Tmarbt@users.noreply.github.com>
2022-12-07 09:12:11 +0100

    Create Operator while.py

---
+++ Operator while.py
@@ -0,0 +1,12 @@
+#tasks from:https://younglinux.info/python/while
+#1st task
+total = 100
+while total > 100:
+    n = int(input())
+    total = total - n
+print("Out of range")
+#2nd task
+a = 2
+while a < 2**20:
+    a *= 2
+    print(a)
