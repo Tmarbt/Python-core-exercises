1ba38726ad90ff71d9f53bd22d1ffef2cd37b23e
Artem Stoliar <99268175+Tmarbt@users.noreply.github.com>
2022-12-14 22:08:04 +0100

    Create Function parameters and arguments.py

---
+++ Function parameters and arguments.py
@@ -0,0 +1,15 @@
+#task from:https://younglinux.info/python/arguments
+def getInput():
+    return input()
+def testInput(i):
+    if type(i) == int:
+        return True
+    else:
+        return False
+def strToInt(i):
+    return int(s)
+def printInt(i):
+    print(i)
+i = testInput(getInput())
+if i == True:
+    printInt(strToInt(i))
